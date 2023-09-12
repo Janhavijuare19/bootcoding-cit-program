c39d0012d94ebe4666839a6f2fcdf8e1d75399c1
Janhavijuare19 <144772711+Janhavijuare19@users.noreply.github.com>
2023-09-12 16:04:02 +0530

    Add files via upload
    java codes

---
+++ AdditionProgram.java
@@ -0,0 +1,11 @@
+public class AdditionProgram
+{
+public static void main(String args[])
+{
+ System.out.println("My first program in java");
+ int a=88,b=90;
+ int c;
+ c=a+b;
+ System.out.println(c);
+ }
+ }
---
+++ IfElse.java
@@ -0,0 +1,22 @@
+public class IfElse
+{
+ public static void main(String args[])
+ {
+ //find out the largest number from given three numbers
+  int a=20,b=30,c=10;
+  if(a<b && a<c)
+  {
+  System.out.println("The greater number is a");
+  } 
+  else if(b>a && b>a)
+  {
+  System.out.println("The greater number is b");
+  }
+  else
+  {
+  System.out.println("The greater number is c");
+  }
+  }
+  }
+ 
+ 
